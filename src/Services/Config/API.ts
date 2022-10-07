10977046b00a13319827540b5bb9f93ec8e1a94c
Carlos Araujo <carlos.araujo@ketra.com.br>
2022-10-07 11:40:05 -0300

    alteração de baseURL

--- src/Services/Config/API.ts
+++ src/Services/Config/API.ts
@@ -2,10 +2,10 @@ import Axios from 'axios';
 
 
 const api = Axios.create({
-    baseURL: 'http://localhost:9000/api/'
+    baseURL: 'https://localhost:7205/api/'
 });
 
-//'http://ec2-107-21-192-8.compute-1.amazonaws.com:9000/api/'
+//'http://ec2-107-21-192-8.compute-1.amazonaws.com/api/'
 api.interceptors.request.use((config: any) => {
     if(localStorage.getItem("@tokenWa"))
         config.headers.Authorization = `Bearer ${localStorage.getItem("@tokenWa")}`;
